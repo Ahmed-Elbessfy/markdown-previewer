a744f0eb7d93be3b4998177a373f6ad6232063aa
Ahmed-Elbessfy <ahmedelbessfy@gmail.com>
2019-02-13 14:13:50 +0200

    add Previewer.js component and import it in App.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React, { Component } from 'react';
 import './App.css';
 
-import Editor from './components/Editor'
+import Editor from './components/Editor';
+import Previewer from './components/Previewer';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Editor />
+        <Previewer />
       </div>
     );
   }
---
+++ src/components/Previewer.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Previewer extends Component {
+  render() {
+    return (
+      <div>
+        <p id='previewer'></p>
+      </div>
+    );
+  }
+}
+
+export default Previewer;
