37392b8b4b3cbacd877c18dfb01ba02d351896cc
Ahmed-Elbessfy <ahmedelbessfy@gmail.com>
2019-02-13 14:11:28 +0200

    add Editor.js component and import it in App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import './App.css';
 
+import Editor from './components/Editor'
+
 class App extends Component {
   render() {
     return (
       <div className="App">
-        
+        <Editor />
       </div>
     );
   }
---
+++ src/components/Editor.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class Editor extends Component {
+  render() {
+    return (
+      <form id='editor'>
+        <textarea>
+          
+        </textarea>
+      </form>
+    );
+  }
+}
+
+export default Editor;
