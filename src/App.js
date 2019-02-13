62922ac54c47e9eca4b8bdf44f92ad6fa4164761
Ahmed-Elbessfy <ahmedelbessfy@gmail.com>
2019-02-13 14:38:42 +0200

    update app state with textarea content from Editor component
    - add handlehange function to textarea element in Editor component so we can get its content
    - add construtor to Editor component and bind the hndelchange to the component
    - create handleChange function so it pass the textarea content to the markdownCode property
    
    - add constructor to app compnent
    - add state markdownCode to app component
    - bind getMarkdownCode to app component
    - create getMarkdownCode so it update the state markdownCode with the value passed from the Editor markdownCode property
    - save the getMarkdownCode function to the Editor property markdownCode

--- src/App.js
+++ src/App.js
@@ -5,10 +5,22 @@ import Editor from './components/Editor';
 import Previewer from './components/Previewer';
 
 class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      markdownCode : ''
+    }
+    this.getMarkdownCode = this.getMarkdownCode.bind(this)
+  }
+  getMarkdownCode(code){
+    this.setState({
+      markdownCode : code
+    })
+  }
   render() {
     return (
       <div className="App">
-        <Editor />
+        <Editor markdownCode={this.getMarkdownCode}/>
         <Previewer />
       </div>
     );
--- src/components/Editor.js
+++ src/components/Editor.js
@@ -1,10 +1,17 @@
 import React, { Component } from 'react';
 
 class Editor extends Component {
+  constructor(props){
+    super(props);
+    this.handleChange = this.handleChange.bind(this)
+  }
+  handleChange(e){
+    this.props.markdownCode(e.target.value)
+  }
   render() {
     return (
       <form id='editor'>
-        <textarea>
+        <textarea onChange={this.handleChange}>
           
         </textarea>
       </form>
