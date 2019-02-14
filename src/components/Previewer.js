d16ade1f9aebab2f14423e5feb65ae529808fabf
Ahmed-Elbessfy <ahmedelbessfy@gmail.com>
2019-02-14 07:06:37 +0200

    add convertMarkdown fucntion
    create convertMarkdown fucntion to Preview component to convert Markdown code to Html code

--- src/components/Previewer.js
+++ src/components/Previewer.js
@@ -1,9 +1,17 @@
-import React from 'react'
+import React from 'react';
+import marked from 'marked';
 
 function Previewer(props) {
+
+  //https://medium.com/@arpith/handling-markdown-in-react-24b275cddf39
+  //Convert Markdown to html code function
+  const convertMarkdown = () => {
+    let returnedCode = marked(props.htmlCode, {sanitize : true});
+    return {__html : returnedCode}
+  }
   return (
     <div>
-      <p id='previewer'>{props.htmlCode}</p>
+      <p id='previewer' dangerouslySetInnerHTML={convertMarkdown()} />
     </div>
   )
 }
