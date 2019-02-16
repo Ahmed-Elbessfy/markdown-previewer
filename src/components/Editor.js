eafd0ed88b56dc9b37bef43d8965572b4f32143f
Ahmed-Elbessfy <ahmedelbessfy@gmail.com>
2019-02-16 16:43:01 +0200

    add auto compile option
    - add ref to textarea in  Editor component
    - store default value of textarea in the markdownCode property

--- src/components/Editor.js
+++ src/components/Editor.js
@@ -3,15 +3,20 @@ import React, { Component } from 'react';
 class Editor extends Component {
   constructor(props){
     super(props);
+    this.textarea = React.createRef();
     this.handleChange = this.handleChange.bind(this)
   }
   handleChange(e){
     this.props.markdownCode(e.target.value)
   }
+  componentDidMount(){
+    this.props.markdownCode(this.textarea.current.defaultValue)
+  }
   render() {
     return (
       <form id='editor'>
         <textarea 
+          ref = {this.textarea}
           onChange={this.handleChange}
           style={{width:'100%', height : '20rem' }}
           defaultValue='# header1'
