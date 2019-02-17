f9f44519cc330377b1e05f9468bc16e19fda5dab
Ahmed-Elbessfy <ahmedelbessfy@gmail.com>
2019-02-17 13:33:46 +0200

    add default content
    - modify textarea defaultValue in the Editor component and add the default content to be displayed once the app is loaded

--- src/components/Editor.js
+++ src/components/Editor.js
@@ -10,16 +10,41 @@ class Editor extends Component {
     this.props.markdownCode(e.target.value)
   }
   componentDidMount(){
+
     this.props.markdownCode(this.textarea.current.defaultValue)
   }
   render() {
+    const defaultContent = `# FreeCodeCamp Markdown Previewer \n\n _this a default content but you can change it_ \n\n ## Welcome to my second project for FreeCodeCamp \n\n this a project for the Frotn-End libraries certificate at [FreeCodeCamp]() curriculium. \n\n ### Examples of markdown syntax: \n\n You can use this as reference to markdown syntax \n\n # heading1 \n ## heading \n ### heading3 \n #### heading4 \n ##### heading5 \n ###### heading6 \n\n --- \n #### plain paragraph \n\n This a plain paragraph.This a plain paragraph.This a plain paragraph.This a plain paragraph. \n \n --- \n #### Text Styles\n\n we can create text-styles like :
+    \n *Italic Text* using astroids \* Itlaic Text \* or _Italic Text too_ using underscores \_ Italic Text \_ \n\n **Bold Text** using double astroids \*\* Bold Text \*\* or __Bold Text too__ using double underscores \_\_ Bold Text \_\_  \n\n ~~Deleted Text~~ using hyphens \~\~ Deleted Text \~\~  \n \n --- \n #### Links \n\n This is a [link](#) \n \n --- \n #### Images \n\n ![FreeCodeCamp](https://markdown-here.com/img/icon128.png) \n \n --- \n #### Lists 
+    \n Here is some lists : \n\n * ul-list
+    * ul-list-item
+    * ul-list-item \n * ol-list
+    1. ol-list-item
+    2. ol-list-item
+    3. ol-list-item \n * list-with-nested-items
+      - list item
+      - list parent
+          1. list-child
+          2. list-child
+            * list-child-second
+            * list-child-second
+      - list parent two
+          1. list-child
+          2. list-child
+   \n --- \n ##### Blockquotes \n\n > blockquote of a wise person \n\n --- \n #### Codeblocks \n\n you can write codeblocks too: \n\n \`\`\`javascript
+     // multi-line code block
+         const functionName = (args) => {
+          return args.split('')
+        }
+      \`\`\` \n\n --- \n\n #### Tables \n\n|header | header |header | header |header |\n|-------|--------|-------|--------|-------|\n|body   | body   | body  | body   | body  |\n|body   | body   | body  | body   | body  |\n|body   | body   | body  | body   | body  |\n|body   | body   | body  | body   | body  |
+  `
     return (
       <form id='editor'>
-        <textarea 
+        <textarea
           ref = {this.textarea}
           onChange={this.handleChange}
           style={{width:'100%', height : '20rem' }}
-          defaultValue='# header1'
+          defaultValue={defaultContent}
         >
         </textarea>
       </form>
