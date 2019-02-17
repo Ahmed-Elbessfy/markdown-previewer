6868124e0c4c1333dc9c107d778236dd028eee79
Ahmed-Elbessfy <ahmedelbessfy@gmail.com>
2019-02-17 13:38:27 +0200

    add autofocus and some spaces
    - allow autofocus on the Textarea on the app loaded
    - add spaces to beautify the output code

--- src/components/Editor.js
+++ src/components/Editor.js
@@ -10,11 +10,11 @@ class Editor extends Component {
     this.props.markdownCode(e.target.value)
   }
   componentDidMount(){
-
-    this.props.markdownCode(this.textarea.current.defaultValue)
+    this.props.markdownCode(this.textarea.current.defaultValue);
+    this.textarea.current.focus();
   }
   render() {
-    const defaultContent = `# FreeCodeCamp Markdown Previewer \n\n _this a default content but you can change it_ \n\n ## Welcome to my second project for FreeCodeCamp \n\n this a project for the Frotn-End libraries certificate at [FreeCodeCamp]() curriculium. \n\n ### Examples of markdown syntax: \n\n You can use this as reference to markdown syntax \n\n # heading1 \n ## heading \n ### heading3 \n #### heading4 \n ##### heading5 \n ###### heading6 \n\n --- \n #### plain paragraph \n\n This a plain paragraph.This a plain paragraph.This a plain paragraph.This a plain paragraph. \n \n --- \n #### Text Styles\n\n we can create text-styles like :
+    const defaultContent = ` # FreeCodeCamp Markdown Previewer \n\n _this a default content but you can change it_ \n\n ## Welcome to my second project for FreeCodeCamp \n\n this a project for the Frotn-End libraries certificate at [FreeCodeCamp]() curriculium. \n\n ### Examples of markdown syntax: \n\n You can use this as reference to markdown syntax \n\n # heading1 \n ## heading \n ### heading3 \n #### heading4 \n ##### heading5 \n ###### heading6 \n\n --- \n #### plain paragraph \n\n This a plain paragraph.This a plain paragraph.This a plain paragraph.This a plain paragraph. \n \n --- \n #### Text Styles\n\n we can create text-styles like :
     \n *Italic Text* using astroids \* Itlaic Text \* or _Italic Text too_ using underscores \_ Italic Text \_ \n\n **Bold Text** using double astroids \*\* Bold Text \*\* or __Bold Text too__ using double underscores \_\_ Bold Text \_\_  \n\n ~~Deleted Text~~ using hyphens \~\~ Deleted Text \~\~  \n \n --- \n #### Links \n\n This is a [link](#) \n \n --- \n #### Images \n\n ![FreeCodeCamp](https://markdown-here.com/img/icon128.png) \n \n --- \n #### Lists 
     \n Here is some lists : \n\n * ul-list
     * ul-list-item
@@ -36,8 +36,7 @@ class Editor extends Component {
          const functionName = (args) => {
           return args.split('')
         }
-      \`\`\` \n\n --- \n\n #### Tables \n\n|header | header |header | header |header |\n|-------|--------|-------|--------|-------|\n|body   | body   | body  | body   | body  |\n|body   | body   | body  | body   | body  |\n|body   | body   | body  | body   | body  |\n|body   | body   | body  | body   | body  |
-  `
+      \`\`\` \n\n --- \n\n #### Tables \n\n |header | header |header | header |header |\n |-------|--------|-------|--------|-------|\n |body   | body   | body  | body   | body  |\n |body   | body   | body  | body   | body  |\n |body   | body   | body  | body   | body  |\n |body   | body   | body  | body   | body  |\n `
     return (
       <form id='editor'>
         <textarea
