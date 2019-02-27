411655e2b5af91cf596c8e74be5cf80276229a56
Ahmed-Elbessfy <ahmedelbessfy@gmail.com>
2019-02-27 19:40:07 +0200

    Updates

---
+++ precache-manifest.14f96c4327e6ba3701224c0d44667cd3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "d1c7fdd19b32b1a54c00",
+    "url": "/markdown-previewer/static/js/runtime~main.d1c7fdd1.js"
+  },
+  {
+    "revision": "b60fc2d88952012dc437",
+    "url": "/markdown-previewer/static/js/main.b60fc2d8.chunk.js"
+  },
+  {
+    "revision": "c01af45d67e5d2e4a98f",
+    "url": "/markdown-previewer/static/js/2.c01af45d.chunk.js"
+  },
+  {
+    "revision": "b60fc2d88952012dc437",
+    "url": "/markdown-previewer/static/css/main.5421be08.chunk.css"
+  },
+  {
+    "revision": "b2e08a0a90ec1feaf3b73e4958fa278a",
+    "url": "/markdown-previewer/index.html"
+  }
+];
